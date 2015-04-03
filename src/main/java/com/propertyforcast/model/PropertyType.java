b7f03157f5e579bb35199e8679df9882c094151a
Andrew Haines <ajh21_48@hotmail.com>
2015-04-03 15:18:55 +0100

    Adding missing property types

--- src/main/java/com/propertyforcast/model/PropertyType.java
+++ src/main/java/com/propertyforcast/model/PropertyType.java
@@ -10,12 +10,14 @@ public enum PropertyType {
 	FLAT("flat"),
 	MAISONETTE("Maisonette"),
 	BUNGALOW("Bungalow"),
+	TERRACED_BUNGALOW("Terraced bungalow"),
 	TOWN_HOUSE("Town house"),
 	COTTAGE("Cottage"),
 	FARM_BARN("Farm/Barn"),
 	MOBILE_STATIC("Mobile/static"),
 	MOBILE_PARK_HOME("Mobile/park home"),
 	BARN_FARM_HOUSE("Barn conversion/farmhouse"),
+	BARN_CONVERSION("Barn conversion"),
 	LINKED_DETACHED_HOUSE("Link-detached house"),
 	LAND("Land"),
 	STUDIO("Studio"),
@@ -23,7 +25,13 @@ public enum PropertyType {
 	PARKING_GARAGE("Parking/garage"),
 	HOUSE_BOAT("Houseboat"),
 	FARM("Farm"),
+	RETAIL_PREMISES("Retail premises"),
+	RESTURANT_CAFE("Restaurant/cafe"),
 	FARMHOUSE("Farmhouse"),
+	DETACHED_BUNGALOW("Detached bungalow"),
+	SEMI_DETECHED_BUNGALOW("Semi-detached bungalow"),
+	VILLA("Villa"),
+	CHALET("Chalet"),
 	OFFICE("Office");
 	
 	private final String typeId;
